914d0103ed14f7d1692c640feaf3602add52d52b
Sagar Devkota <30004439@student.toiohomai.ac.nz>
2019-03-06 11:51:49 +1300

    practise

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            
+
+            Console.WriteLine("print your name");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("print your age");
+            int age1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("print your gender");
+            string gender1 = Console.ReadLine();
+            Console.WriteLine("print your adress");
+            string adress1 = Console.ReadLine();
+
+            Console.WriteLine("Name: " + name1);
+            Console.WriteLine("Age: " + age1);
+            Console.WriteLine("Gender: " + gender1);
+            Console.WriteLine("Adress: " + adress1);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+            Console.Write("Please enter a number: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.Write("Please enter another number: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double result;
+            result = num1 + num2;
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, result);
+
+            result = num1 - num2;
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, result);
+
+            result = num1 * num2;
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, result);
+
+            result = num1 / num2;
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+            string fname;
+            string lname;
+          
+            Console.WriteLine("your first name");
+            fname = Console.ReadLine();
+
+            Console.WriteLine("your last name");
+            lname = Console.ReadLine();
+            Console.WriteLine(fname+" "+lname);
+        }
+    }
+}
